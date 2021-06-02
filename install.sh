6be6b54a11d83bcf27b00d52613bcd12ffb93796
Jack Anderson <jack@jda.ac>
2021-06-02 20:10:02 +0100

    Update README.md

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 cp config.sample.json config.json
 npm install --unsafe-perm=true --allow-root
-cd player || { echo "Directory player missing"; exit 1; }
+cd player || { echo "Directory 'player' missing"; exit 1; }
 npm install --unsafe-perm=true --allow-root
 npm run build --unsafe-perm=true --allow-root
-cd ../dashboard || { echo "Directory ../dashboard missing"; exit 1; }
+cd ../dashboard || { echo "Directory '../dashboard' missing"; exit 1; }
 npm install --unsafe-perm=true --allow-root
 npm run build --unsafe-perm=true --allow-root
